7e41f9d089aebcf47ef6a9b195cc88453fd6c21a
diogoeverson <diogoeverson@gmail.com>
2020-01-30 17:00:49 -0300

    Versão 2.0 da análise

--- AnaliseFraudeTKD.R
+++ AnaliseFraudeTKD.R
@@ -3,7 +3,7 @@
 #https://www.kaggle.com/c/talkingdata-adtracking-fraud-detection/data
 
 #setup working directory
-setwd("C:/FCD/BigDataRAzure/Cap20/Projeto01")
+setwd("C:/FCD/BigDataRAzure/Cap20/Projeto01/Data-Sci-ML-R-TalkingData-FraudDetection-Case")
 getwd()
 
 #carga de pacotes
@@ -16,64 +16,119 @@ library(randomForest)
 install.packages('e1071', dependencies=TRUE)
 library(e1071)
 
-#PRE PROCESSAMENTO + #ANALISE EXPLORATORIA
+#ANÁLISE EXPLORATORIA + PRÉ-PROCESSAMENTO
+#(remoção de outliers, normalização(colocar os dados na mesma escala), redução de dimensionalidade(feature selection))
 
-#Importa dados para datasets - trabalhando somente com amostra dos dados de treino devido limita��es do desktop
+#Importa dados para datasets - trabalhando somente com amostra dos dados de treino devido limitações do desktop
 
-treino <- read.csv("train_sample.csv", header = TRUE, sep = ",")
+treino <- read.csv("../train_sample.csv", header = TRUE, sep = ",")
 
 head(treino)
 View(treino)
 summary(treino)
+
+#ajustando categoria das variáveis
 str(treino)
-?dplyr
 
 count(treino, ip)
-count(treino, app)
-count(treino, device)
-count(treino, os)
-count(treino, channel)
-count(treino, click_time)
-count(treino, attributed_time)
-count(treino, as.factor(is_attributed))
-count(treino, ip)
+treino$ip <- as.numeric(treino$ip)
 
+count(treino, app) 
+treino$app <- as.numeric(treino$app)
 
-#Ajustes vari�veis do tipo Fator e Data e hora
+count(treino, device) 
+treino$device <- as.numeric(treino$device)
+
+count(treino, os) 
+treino$os <- as.numeric(treino$os)
+
+count(treino, channel) 
+treino$channel <- as.numeric(treino$channel)
+
+count(treino, click_time) 
+treino$click_time <- as.numeric(treino$click_time)
+
+count(treino, attributed_time) 
+treino$attributed_time <- as.numeric(treino$attributed_time)
+
+count(treino, as.factor(is_attributed)) #fator target
+treino$is_attributed <- as.factor(treino$is_attributed)
+
 str(treino)
-?factor
-
-treino$click_time = as.Date(treino$click_time)
-treino$attributed_time = as.Date(treino$attributed_time != NA)
-
-str(treino)
-View(treino)
-
-#demais ajustes vari�veis
-?dplyr
-
-distinct(treino, is_attributed)
-#Gr�ficos para an�lise explorat�ria histograma e boxplot
-?ggplot2
-
-ggplot(treino, aes(app)) +
-  geom_bar()
-
-ggplot(treino, aes(device)) +
-  geom_bar()
-
-ggplot(treino, aes(os)) +
-  geom_bar()
-
-ggplot(treino, aes(channel)) +
-  geom_bar()
+head(treino)
+#normalização das variáveis
+?scale
+treino$ip <- scale(treino$ip, center=T, scale=T)
+treino$app <- scale(treino$app, center=T, scale=T)
+treino$device <- scale(treino$device, center=T, scale=T)
+treino$os <- scale(treino$os, center=T, scale=T)
+treino$channel <- scale(treino$channel, center=T, scale=T)
+treino$click_time <- scale(treino$click_time, center=T, scale=T)
 
 
-hist(treino$is_attributed, main = "Histograma is_attributed")
+# Dividindo os dados em treino e teste - 60:40 ratio
+indexes <- sample(1:nrow(treino), size = 0.6 * nrow(treino))
+indexes
+train.data <- treino[indexes,]
+test.data <- treino[-indexes,]
 
-boxplot(treino$is_attributed, main = "Boxplot is_attributed")
+# Feature selection
+?rfeControl
+?rfe
 
-#obs. a variavel target esta desbalanceada na amostra de treino.
+control <- rfeControl(functions = rfFuncs, method = "cv", 
+                      verbose = FALSE, returnResamp = "all", 
+                      number = 3)
+
+feature.vars = train.data[,-8]
+class.var = train.data[,8]
+
+variable.sizes <- 1:8
+
+results.rfe <- rfe(x = feature.vars, y = class.var, 
+                   sizes = variable.sizes, 
+                   rfeControl = control)
+
+# Visualizando os resultados
+results.rfe
+#?varImp
+varImp((results.rfe))
+
+# Criando e Avaliando o Modelo
+
+# Construindo um modelo de regressão logística
+formula.init <- "is_attributed ~ ."
+formula.init <- as.formula(formula.init)
+
+summary(treino)
+
+lr.model <- glm(formula = formula.init, data = train.data, family = "binomial")
+
+# Visualizando o modelo
+summary(lr.model)
+
+# Testando o modelo nos dados de teste
+lr.predictions <- predict(lr.model, test.data, type="response")
+lr.predictions <- round(lr.predictions)
+
+# Avaliando o modelo
+## separate feature and class variables
+test.feature.vars <- test.data[,-8]
+test.class.var <- test.data[,8]
+
+confusionMatrix(table(data = lr.predictions, reference = test.class.var), positive = '1')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #APRENDIZADO
